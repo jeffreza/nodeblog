e689a094e15e2601cd61a7ea49242386d81dfd07
jeffreza <jeffreza041992@gmail.com>
2020-06-18 04:59:27 +0500

    jias

--- server.js
+++ server.js
@@ -1,28 +1,41 @@
 const express = require ('express');
 const app = express();
 const articleRouter = require('./routes/articles');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/blog',{useUnifiedTopology: true, useNewUrlParser: true });
 
 app.set('view engine', 'ejs');
 app.use('/articles',articleRouter);
 
-app.get('/',(req,res)=>{
 
-    const articles = [{
 
-        title: 'test article',
-        createdAt: new Date(),
-        description: 'test description'
+app.get('/',(req,res)=>
+{
+    try {
 
-    },
-    {
+        const articles = [{
 
-        title: 'test article2',
-        createdAt: new Date(),
-        description: 'test description'
-
+            title: 'test article',
+            createdAt: new Date(),
+            description: 'test description'
+    
+        },
+        {
+    
+            title: 'test article2',
+            createdAt: new Date(),
+            description: 'test description'
+    
+        }
+    ];
+        res.render('articles/index',{articles:articles});
+        
+    } catch (error) {
+        console.log(error);
     }
-];
-    res.render('index',{articles:articles});
+    
+    
    
 
 });
